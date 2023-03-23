dea2bb403959d60e98102c09506b8a9d77ac8e73
silence48 <35656692+silence48@users.noreply.github.com>
2023-03-23 16:49:08 -0400

    add post and option methods

--- functions/auth.ts
+++ functions/auth.ts
@@ -5,8 +5,10 @@ import {
   BASE_FEE,
   Operation,
   Account,
+  xdr
 } from 'stellar-base';
 import { Buffer } from "buffer-polyfill";
+import { Transaction } from '../node_modules/stellar-base/types/index';
 //found the fix for polyfilling buffer like this from https://github.com/remix-run/remix/issues/2813
 globalThis.Buffer = Buffer as unknown as BufferConstructor;
 
@@ -15,7 +17,7 @@ interface Env {
   authsigningkey: any;
 }
 
-export const onRequest: PagesFunction<Env> = async (context) => {
+export const onRequestGet: PagesFunction<Env> = async (context) => {
     const request = context.request
     const { searchParams } = new URL(request.url);
     const userAccount = searchParams.get('account');
@@ -53,6 +55,38 @@ export const onRequest: PagesFunction<Env> = async (context) => {
     //this should fail
     //console.log(verifyTxSignedBy(transaction, failkey));
 }
+export const onRequestOptions: PagesFunction<Env> = async (context) => {
+
+  return new Response("OK", {
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+      "Access-Control-Allow-Methods":"OPTIONS, POST, GET",
+      "Access-Control-Allow-Origin": "*",
+    },
+  })
+}
+export const onRequestPost: PagesFunction<Env> = async (context) => {
+  type authrequest = {
+    transaction: string
+  }
+  const authjson: authrequest = await context.request.json()
+  //todo: Set the network passphrase as a env var.
+  //todo: build the jwt token
+  let transaction = new TransactionBuilder.fromXDR(authjson.transaction, Networks.TESTNET)
+  const valid = verifyTxSignedBy(transaction,transaction.source)
+  const json = JSON.stringify(transaction, null, 2);
+  return new Response(json, {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+}
+
+async function verifyTxSignedBy(transaction, accountID) {
+  console.log(transaction)
+    return gatherTxSigners(transaction, [accountID]).length !== 0;
+  }
 
 async function generateAuthToken(serverkey, pubkey, discordID): Promise<TransactionBuilder>{
     let tempAccount=new Account(pubkey,"-1");
@@ -69,8 +103,8 @@ async function generateAuthToken(serverkey, pubkey, discordID): Promise<Transact
             .setTimeout(60*60*24*30)
             .build();
     await transaction.sign(serverkey);
-    const token = await transaction.toEnvelope().toXDR('base64');
-    return token;
+    const challenge = await transaction.toEnvelope().toXDR('base64');
+    return challenge;
 }
 
 function gatherTxSigners(transaction, signers) {
