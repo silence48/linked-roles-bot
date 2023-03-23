314e43a791d92a94f89604932e66e434245d6612
silence48 <35656692+silence48@users.noreply.github.com>
2023-03-22 21:58:30 -0400

    update federation

---
+++ functions/federation/index.ts
@@ -0,0 +1,25 @@
+const handler: ExportedHandler = {
+  async fetch(request: Request) {
+    const { searchParams } = new URL(request.url);
+    let q = searchParams.get('q');
+    let t = searchParams.get('type');
+
+    const data = {
+      "stellar_address": "bob*stellar.org",
+      "account_id": "GCIBUCGPOHWMMMFPFTDWBSVHQRT4DIBJ7AD6BZJYDITBK2LCVBYW7HUQ",
+      "memo_type": "id",
+      "memo": "123",
+      "Thequery": q,
+      "the type": t
+    };
+
+    const json = JSON.stringify(data, null, 2);
+
+    return new Response(json, {
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  },
+};
