66bd272e44f9022c8813a166875fc218db34c54e
silence48 <35656692+silence48@users.noreply.github.com>
2023-06-14 18:47:20 -0400

    add a nav

--- app/routes/BadgeGrid.tsx
+++ app/routes/BadgeGrid.tsx
@@ -1,5 +1,5 @@
 import { json, type LoaderArgs } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, Link } from "@remix-run/react";
 import React, { useState, useEffect, useRef } from "react";
 
 // Define your action function
@@ -104,6 +104,7 @@ export default function Index() {
     setIsLoading(false);
     modalRef.current.showModal();
   };
+
   const handleDarkModeToggleEnd = (isDarkMode: boolean) => {
     console.log(`Dark mode is now ${isDarkMode ? "enabled" : "disabled"}`);
   };
@@ -115,10 +116,41 @@ export default function Index() {
   const handleMenuOpen = () => {
     console.log("Opening menu...");
   };
+
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   return (
+    
     <div className="flex flex-wrap justify-center bg-gray-800 p-4 w-full">
+
+<div className="navbar bg-base-100">
+  <div className="flex-1">
+    <Link className="btn btn-ghost normal-case text-xl" to="/">Stellar Linked Roles</Link>
+  </div>
+  <div className="flex-none gap-2">
+    
+    <Link className="btn btn-primary normal-case text-xl" to="/">Login</Link>
+        
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar placeholder">
+        <div className="bg-neutral-focus text-neutral-content rounded-full w-12">
+          
+        </div>
+      </label>
+      <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
       {badgeDetails.map((badge, index) => (
         <div
           className="flex w-full min-w-[120px] max-w-[240px] m-[1vw] sm:m-1"
