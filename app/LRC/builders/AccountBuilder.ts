7c2705a1f5b64a31e6bfbe015fd8fe19b541f408
silence48 <35656692+silence48@users.noreply.github.com>
2023-06-27 19:22:30 -0400

    refactor addaccount

--- app/LRC/builders/AccountBuilder.ts
+++ app/LRC/builders/AccountBuilder.ts
@@ -3,6 +3,18 @@ import { UserForm, StellarAccountForm } from '../forms';
 import { Keypair } from 'stellar-base';
 import type { UserDataI } from '../interfaces/types';
 
+export interface AccountBuilder {
+  discord_user_id: string;
+  data: any;
+  DB: any;
+  setup(user: UserDataI): Promise<void>;
+  build(): Promise<void>;
+  updateDiscordCredentials(credentials: UserDataI): Promise<void>;
+  addStellarAccount(account: { public_key: string, access_token: string, refresh_token: string }): Promise<void>;
+  removeStellarAccount(account: { public_key: string }): Promise<void>;
+  getStellarAccounts(): Promise<{ accounts: any[] }>;
+}
+
 interface AccountBuilderData {
   discord_user_id?: string;
   data?: any;
@@ -10,7 +22,7 @@ interface AccountBuilderData {
 }
 
 export class AccountBuilder {
-  readonly DB: any;
+  DB: any;
   data: any;
   discord_user_id: string;
 
@@ -21,37 +33,30 @@ export class AccountBuilder {
   }
 
   static async create({ user, DB }: { user: UserDataI; DB: any }) {
-    const { discord_user_id, discord_access_token, discord_expires_at, discord_refresh_token } = user;
-    const instance = new AccountBuilder({ discord_user_id, DB });
-    if (instance.discord_user_id) {
-      await instance.setup({ discord_user_id, discord_access_token, discord_expires_at, discord_refresh_token });
-    }
+    const instance = new AccountBuilder({ discord_user_id: user.discord_user_id, DB });
+    await instance.setup(user);
     return instance;
   }
 
   static async find({ discord_user_id, DB }: { discord_user_id: string; DB: any }) {
     const instance = new AccountBuilder({ discord_user_id, DB });
-    if (instance.discord_user_id) {
-      await instance.build();
-    }
+    await instance.build();
     return instance;
   }
 
   async setup(user: UserDataI) {
-    const { DB } = this;
     const userForm = new UserForm(new User(user));
-    const { id } = (await User.create(userForm, DB)) ?? {};
+    const { id } = (await User.create(userForm, this.DB)) ?? {};
     if (id) {
-      return await this.build();
+      await this.build();
     } else {
       console.log('something went wrong');
     }
   }
 
-  private async build() {
-    const { DB, discord_user_id } = this;
-    const user = await User.findOne('discord_user_id', discord_user_id, DB);
-    const accounts = (await StellarAccount.findBy('discord_user_id', discord_user_id, DB)) ?? [];
+  async build() {
+    const user = await User.findOne('discord_user_id', this.discord_user_id, this.DB);
+    const accounts = (await StellarAccount.findBy('discord_user_id', this.discord_user_id, this.DB)) ?? [];
     this.data = {
       user,
       accounts
@@ -59,8 +64,7 @@ export class AccountBuilder {
   }
 
   async updateDiscordCredentials({ discord_access_token, discord_refresh_token, discord_expires_at }: UserDataI) {
-    const { DB, discord_user_id } = this;
-    const user = await User.findOne('discord_user_id', discord_user_id, DB);
+    const user = await User.findOne('discord_user_id', this.discord_user_id, this.DB);
     await User.update(
       {
         ...user,
@@ -68,41 +72,40 @@ export class AccountBuilder {
         discord_refresh_token,
         discord_expires_at
       },
-      DB
+      this.DB
     );
-    return await this.build();
+    await this.build();
   }
 
-  async addStellarAccount({ public_key }: any) {
-    const { DB, discord_user_id } = this;
+  async addStellarAccount({ public_key, access_token, refresh_token }: any) {
     const key = Keypair.fromPublicKey(public_key).publicKey();
-    const exists = await StellarAccount.findOne('public_key', key, DB)
+    const exists = await StellarAccount.findOne('public_key', key, this.DB)
     
     if (!exists) {
       const stellarForm = new StellarAccountForm(
         new StellarAccount({
           public_key: key,
-          discord_user_id: discord_user_id
+          discord_user_id: this.discord_user_id,
+          access_token: access_token,
+          refresh_token: refresh_token
         })
       );
-      await StellarAccount.create(stellarForm, DB);
+      await StellarAccount.create(stellarForm, this.DB);
     }
 
-    return await this.build();
+    await this.build();
   }
 
   async removeStellarAccount({ public_key }: any) {
-    const { DB, discord_user_id } = this;
-    const stellarAccount = await StellarAccount.findOne('public_key', public_key, DB);
-    if (stellarAccount.discord_user_id === discord_user_id) {
-      await StellarAccount.delete(stellarAccount.id, DB);
-      return await this.build();
+    const stellarAccount = await StellarAccount.findOne('public_key', public_key, this.DB);
+    if (stellarAccount.discord_user_id === this.discord_user_id) {
+      await StellarAccount.delete(stellarAccount.id, this.DB);
+      await this.build();
     }
   }
 
   async getStellarAccounts() {
-    const { DB, discord_user_id } = this;
-    const accounts = (await StellarAccount.findBy('discord_user_id', discord_user_id, DB)) ?? [];
+    const accounts = (await StellarAccount.findBy('discord_user_id', this.discord_user_id, this.DB)) ?? [];
     return {
       accounts
     };
--- app/routes/challenge.verify.tsx
+++ app/routes/challenge.verify.tsx
@@ -1,3 +1,99 @@
+import { type ActionArgs } from "@remix-run/cloudflare";
+export interface StellarAccountsDataI {
+  id?: string;
+  discord_user_id?: string;
+  public_key?: string;
+  access_token?: string;
+  refresh_token?: string;
+}
+export interface UserDataI {
+  id?: string;
+  discord_user_id?: string;
+  discord_access_token?: string;
+  discord_refresh_token?: string;
+  discord_expires_at?: string;
+}
+
+interface AccountBuilderI {
+  discord_user_id: string;
+  data: any;
+  DB: any;
+  setup: (user: UserDataI) => Promise<void>;
+  build: () => Promise<void>;
+  updateDiscordCredentials: (user: UserDataI) => Promise<void>;
+  addStellarAccount: (account: any) => Promise<void>;
+  removeStellarAccount: (account: any) => Promise<void>;
+  getStellarAccounts: () => Promise<{ accounts: any[] }>;
+}
+
+export async function action({ request, context }: ActionArgs) {
+  const jwt = await import("@tsndr/cloudflare-worker-jwt");
+  const { AccountBuilder } = await import("~/LRC/builders/AccountBuilder");
+  const { updateUserSession, getUser } = await import("~/utils/session.server");
+  const { parse } = await import("cookie");
+  const { TransactionBuilder, Networks } = await import("stellar-base");
+  const { getRefreshToken, getAccessToken } = await import("~/utils/auth.server");
+
+  const { sessionStorage } = context as any;
+  const body = await request.formData();
+  const signedEnvelope = body.get("signed_envelope_xdr");
+  const url = new URL(request.url);
+  const provider = url.searchParams.get("provider") as any;
+  const addAccount = (url.searchParams.get("addAccount") as any) ?? {};
+  const cookies = request.headers.get("Cookie") ?? null;
+  if (!cookies) throw new Error("No cookies found");
+  const cookieHeader = parse(cookies);
+  const { clientState } = cookieHeader;
+  const { discord_user_id } = (await getUser(request, sessionStorage)) ?? {};
+
+  let areq = {
+    Transaction: signedEnvelope,
+    NETWORK_PASSPHRASE: Networks.PUBLIC,
+    discord_user_id: discord_user_id
+  };
+  const { Transaction, NETWORK_PASSPHRASE } = areq;
+  let passphrase: Networks = Networks.PUBLIC;
+  if (NETWORK_PASSPHRASE) {
+    passphrase = NETWORK_PASSPHRASE;
+  }
+
+  const { DB } = context.env as any;
+  let builtTx = new (TransactionBuilder.fromXDR as any)(Transaction, passphrase);
+  const decoder = new TextDecoder();
+  let authedstate = decoder.decode(builtTx.operations[0].value);
+
+  if (clientState !== authedstate) {
+    throw new Error('State verification failed.');
+  }
+
+  const refreshtoken = await getRefreshToken(builtTx, request, context);
+  if (refreshtoken != false) {
+    const accesstoken = await getAccessToken(refreshtoken, request, context);
+    if (accesstoken) {
+      const accountBuilder = await AccountBuilder.find({ discord_user_id, DB });
+      if (!accountBuilder.data.user) {
+        throw new Error('User does not exist.');
+      } else {
+        await accountBuilder.addStellarAccount({ public_key: builtTx.source, access_token: accesstoken, refresh_token: refreshtoken });
+      }
+
+      if (!provider) return;
+      return updateUserSession(
+        request,
+        sessionStorage,
+        { token: accesstoken, provider, account: builtTx.source },
+        {
+          message: 'Challenge verified',
+          body: { account: builtTx.source, provider }
+        }
+      );
+    }
+  } else {
+    throw new Error('Error Verifying Challenge.');
+  }
+}
+
+/*
 import { TransactionBuilder, Networks } from 'stellar-base';
 import { json, type ActionArgs } from '@remix-run/cloudflare';
 import { parse } from 'cookie';
@@ -118,3 +214,4 @@ console.log(`in challenge verify ${discord_user_id}`)
 
   // Store as a Token
 }
+*/
