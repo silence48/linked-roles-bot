c3d9b7d787c7a805f3dff6c281b693105819cac0
silence48 <35656692+silence48@users.noreply.github.com>
2023-06-27 15:45:05 -0400

    allow preview deployment

--- app/routes/verify.tsx
+++ app/routes/verify.tsx
@@ -3,7 +3,7 @@ import { redirect, type LoaderFunction } from "@remix-run/cloudflare";
 export let loader: LoaderFunction = async ({ request, context }) => {
   //const { createUserSession } = await import("~/utils/session.server");
   const { Discord } = await import("linked-roles-core");
-  const discord = await Discord.getOAuthUrl(context.env);
+  const discord = await Discord.getOAuthUrl(request, context.env);
   const { url, state } = discord;
   return redirect(url, {
     status: 301,
