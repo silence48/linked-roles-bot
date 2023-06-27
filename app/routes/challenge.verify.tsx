1714f2ec25b281ba2c1ac4e49ba2df2d0ecc88a5
silence48 <35656692+silence48@users.noreply.github.com>
2023-06-27 18:29:58 -0400

    debug

--- app/routes/challenge.verify.tsx
+++ app/routes/challenge.verify.tsx
@@ -2,8 +2,7 @@ import { TransactionBuilder, Networks } from 'stellar-base';
 import { json, type ActionArgs } from '@remix-run/cloudflare';
 import { parse } from 'cookie';
 import { updateUserSession, getUser } from '~/utils/session.server';
-//import { AccountBuilder, User } from 'linked-roles-core';
-// import jwt from "@tsndr/cloudflare-worker-jwt";
+
 import jwt from "@tsndr/cloudflare-worker-jwt";
 import { AccountBuilder } from '~/LRC/builders/AccountBuilder';
 import { User } from '~/LRC/models/User';
@@ -12,21 +11,33 @@ import { getUrlParams } from '~/utils/getUrlParams';
 import { getRefreshToken, getAccessToken } from '~/utils/auth.server';
 
 export async function action({ request, context }: ActionArgs) {
-  const { sessionStorage, env } = context as any;
-  const { DB, STELLAR_NETWORK, NETWORK_PASSPHRASE } = env;
+  const { sessionStorage } = context as any;
   const body = await request.formData();
+  const signedEnvelope = body.get("signed_envelope_xdr");
+  console.log(`from challenge.verify - action - signedEnvelope ${signedEnvelope}`);
   const url = new URL(request.url);
-  const signedEnvelope = body.get('signed_envelope_xdr');
-  const params = getUrlParams(url);
-  const { provider, addAccount }: any = params ?? {};
-  const cookies = request.headers.get('Cookie') ?? null;
+  const provider = url.searchParams.get("provider") as any;
+  const addAccount = (url.searchParams.get("addAccount") as any) ?? {};
+  const cookies = request.headers.get("Cookie") ?? null;
   if (!cookies) return null;
   const cookieHeader = parse(cookies);
   const { clientState } = cookieHeader;
   const { discord_user_id } = (await getUser(request, sessionStorage)) ?? {};
+console.log(`in challenge verify ${discord_user_id}`)
+  let areq = {
+    Transaction: signedEnvelope,
+    NETWORK_PASSPHRASE: Networks.PUBLIC,
+    discord_user_id: discord_user_id
+  };
+  const { Transaction, NETWORK_PASSPHRASE } = areq;
+  let passphrase: Networks = Networks.PUBLIC;
+  if (NETWORK_PASSPHRASE) {
+    passphrase = NETWORK_PASSPHRASE;
+  }
 
-  let builtTx = new (TransactionBuilder.fromXDR as any)(signedEnvelope, NETWORK_PASSPHRASE);
 
+  const { DB } = context.env as any;
+  let builtTx = new (TransactionBuilder.fromXDR as any)(Transaction, passphrase);
   //verify the state.
   const decoder = new TextDecoder();
   let authedstate = decoder.decode(builtTx.operations[0].value);
@@ -70,11 +81,6 @@ export async function action({ request, context }: ActionArgs) {
           }
         });
       } else {
-        // const user = await User.findBy('discord_user_id', discord_user_id, DB);
-        // user[0].stellar_access_token = accesstoken;
-        // user[0].stellar_refresh_token = refreshtoken;
-        // user[0].stellar_expires_at = (payload.exp).toString();
-        // user[0].public_key = builtTx.source;
 
         const account = await AccountBuilder.find({ discord_user_id, DB });
         await account.addStellarAccount({ public_key: builtTx.source });
